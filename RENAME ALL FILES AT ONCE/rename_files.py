beec7dc55ff423fd921cbfd9eda9c4ef2b17d20c
Devendra Singh <127510525+DevendraSingh7465@users.noreply.github.com>
2023-07-28 16:49:15 +0530

    rename all files name at once

---
+++ RENAME ALL FILES AT ONCE/rename_files.py
@@ -0,0 +1,23 @@
+# rename all files at once usign os module
+import os
+import shutil
+# path = "E:\demo folder\\"
+path = input("Enter Path : ")
+path = path+"\\"
+path.replace('\\','/')
+print(path)
+files = os.listdir(path)
+# print(files)
+i = 0
+def Rename():
+    global i
+    extention = input("Enter Extention Name (example -> .png) : ")
+    for filename in files:
+        new_name = path+str(i)+extention
+        old_name= path + filename
+        os.rename(old_name,new_name)
+        i  += 1
+Rename()
+print(f"Total Files Found : {i}")
+print("All Files Renamed")
+
